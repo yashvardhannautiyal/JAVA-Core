4ce5cc4f4b071c03fd60db3c33ebd57211456188
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-06-11 17:04:47 +0530

    loops

---
+++ 4_loops/loops.java
@@ -0,0 +1,34 @@
+public class loops {
+    public static void main(String[] args){
+//        ----------for loop------------
+//   Syntax:     for(initialize; condition; increment)
+//        for(int i =1;i<=5;i++){
+//            System.out.println(i);
+//        }
+
+//        -------------while loop-------------
+//Syntax :
+// initialize;
+//while(condition){
+    //code
+//increment}
+//        int i =1;
+//        while(i<=5){
+//            System.out.println(i);
+//            i++;}
+//    }
+
+
+//    --------------- do-while loop -----------
+//     Syntax:
+//     initialize;
+//        do{
+//            code;
+//        increment} while(condition);
+
+        int i =0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<=5); }
+}
