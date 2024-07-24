a8f991b90d80fabfb0d6c25fb7c74f862c33774e
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-07-24 12:54:49 +0530

    operators completed

--- 1_introduction/operators.java
+++ 1_introduction/operators.java
@@ -1,5 +1,53 @@
 public class operators {
 public static void main(String[] args){
-    System.out.println("Hello world");
+
+    int a = 5;
+    int b= 6;
+//   ARTITHMETIC OPERATORS
+//    System.out.println(a+b);
+//    System.out.println(a-b);
+//    System.out.println(a*b);
+//    System.out.println(b/a);
+//    System.out.println(b%a);
+
+//    UNARY OPERATORS
+//    System.out.println(a++); //5
+//    System.out.println(b--);//6
+//    System.out.println(++a);//7
+//    System.out.println(--b);//4
+
+//    ASSIGNMENT OPERATOR
+//    System.out.println(a+=b);
+//    System.out.println(a-=b);
+//    System.out.println(a*=b);
+//    System.out.println(b/=a);
+//    System.out.println(b%=a);
+
+
+//    RELATIONAL OPERATOR
+//    System.out.println(a==b);
+//    System.out.println(a!=b);
+//    System.out.println(a>b);
+//    System.out.println(a<b);
+//    System.out.println(a>=b);
+//    System.out.println(a<=b);
+
+//    LOGICAL OPERATOR
+//    if(a<b&& b>a){          LOGICAL AND
+//        System.out.println("true");
+//    }
+
+//    if(a>b || b>a){         LOGICAL OR
+//        System.out.println(true);
+//    }
+
+//    System.out.println(!(a>b));    LOGICAL NOT
+
+
+//    TERNARY OPERATOR
+//boolean result = (a>b) ? true : false;
+//    System.out.println(result);
+
+
 }
 }
