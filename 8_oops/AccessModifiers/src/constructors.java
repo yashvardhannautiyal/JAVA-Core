f0032387b47de47b98286a851b88a9e40a637d8b
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-11 17:43:57 +0530

    constructors completed

--- 8_oops/AccessModifiers/src/constructors.java
+++ 8_oops/AccessModifiers/src/constructors.java
@@ -2,11 +2,30 @@
 class func{
     int id;
     String name;
+    public func(String n, int i){
+        id = i;
+        name = n;
+    }
+//    constructor overloading
+    public func(double h){
+        System.out.println(h);
+    }
+
+    public String getName(){return name;}
+    public void setName(String s){this.name = s;}
+    public int getId(){return id;}
+    public void setId(int d){id = d;}
 }
 
 
 public class constructors {
     public static void main(String[] args) {
+        //direct invoking the data
+//        func hello = new func("helloWorld!",88);
+        func hello = new func(3.14);
+        System.out.println(hello.getName());
+        System.out.println(hello.getId());
+
 
     }
 }
