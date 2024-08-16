7249d1cb06c7a2e2659c9bbb2deec531fe779105
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-16 21:35:39 +0530

    update method overriding

--- 8_oops/Inheritance/src/method_overriding.java
+++ 8_oops/Inheritance/src/method_overriding.java
@@ -1,5 +1,31 @@
+class A{
+    public int a;
+    public int b(){
+        return 1;
+    }
+
+    public void meth2(){
+        System.out.println("I am method2 of class A");
+    }
+
+}
+
+class B extends A{
+    @Override //denotes that the method is overrided
+    public void meth2(){
+        System.out.println("I am method2 of class B");//we can change items in method overriding
+    }
+    public void meth3(){
+        System.out.println("I am method 3 of class B");
+    }
+}
+
 public class method_overriding {
     public static void main(String[] args) {
-        
+    A a = new A();
+    a.meth2();
+
+    B b = new B();
+    b.meth2();
     }
 }
