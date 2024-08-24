ab3d262ff065d1de3ea9c82672b78385fff9e629
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-24 17:18:40 +0530

    basic concept of abstract class

--- 8_oops/abstract_class_and_interfaces/src/abstractClass.java
+++ 8_oops/abstract_class_and_interfaces/src/abstractClass.java
@@ -1,5 +1,31 @@
+//abstract class and interfaces
+//syntax for abstract class and method
+abstract class parent{
+    public parent(){
+        System.out.println("I am base constructor");
+    }
+    public void hello(){
+        System.out.println("hello");
+    }
+    abstract public void greet();
+
+}
+
+class child extends parent{
+    //child class must use greet absrtract method either the child class should be an abstract class
+    @Override
+        public void greet(){
+        System.out.println("good morning");
+    }
+}
+
+
+
 public class abstractClass {
     public static void main(String[] args) {
-        System.out.println("hello world");
+//        we cannot create abstract class object
+//        parent p = new parent();
+        child c = new child();
+        c.greet();
     }
 }
