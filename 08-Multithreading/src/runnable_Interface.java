8befc15c36575170ec39b0a5ce065a3904de7c58
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-30 23:48:13 +0530

    update runnable_interface.java
    completed learning how to create threads using implements runnable interface

--- 08-Multithreading/src/runnable_Interface.java
+++ 08-Multithreading/src/runnable_Interface.java
@@ -1,8 +1,69 @@
 //creating thread using runnable interface method
 
+class myRunnableThread implements Runnable{
+    public void run(){
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+        System.out.println("i am a thread");
+    }
+}
+class myRunnableThread2 implements Runnable{
+    public void run(){
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+        System.out.println("i am a thread2");
+    }
+}
 
 public class runnable_Interface {
     public static void main(String[] args) {
         System.out.println("Runnable Interface");
+//        runnable interface cannot directly use start() method
+        myRunnableThread bullet1 = new myRunnableThread();
+        Thread gun1 = new Thread(bullet1);
+
+        myRunnableThread2 bullet2 = new myRunnableThread2();
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
+
     }
 }
