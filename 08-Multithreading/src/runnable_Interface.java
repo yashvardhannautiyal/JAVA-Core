161d25ac4ae41efa56630ffffc77140d8fde357b
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-30 22:50:19 +0530

    create runnable.java
    started learning how to create a thread using runnable method

---
+++ 08-Multithreading/src/runnable_Interface.java
@@ -0,0 +1,8 @@
+//creating thread using runnable interface method
+
+
+public class runnable_Interface {
+    public static void main(String[] args) {
+        System.out.println("Runnable Interface");
+    }
+}
