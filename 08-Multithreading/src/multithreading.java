f85c10a227b415384ad2ed381caa432900eec99b
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-30 22:46:32 +0530

    extends Thread method completed

--- 08-Multithreading/src/multithreading.java
+++ 08-Multithreading/src/multithreading.java
@@ -1,5 +1,37 @@
+//Creating thread using : extends Thread method
+
+class myThread extends Thread{
+    @Override
+    public void run(){
+        int i = 0;
+    while(i < 80){
+        System.out.println("My thread is running");
+        System.out.println("i am happy");
+        i++;
+    }
+    }
+}
+
+class myThread2 extends Thread{
+    @Override
+    public void run(){
+        int i = 0 ;
+        while(i< 80){
+            System.out.println("myThread2 is running");
+            System.out.println("i am happy2");
+            i++;
+        }
+    }
+}
+
+
 public class multithreading {
     public static void main(String[] args) {
         System.out.println("multithreading");
+        myThread t1 = new myThread();
+        myThread t2 = new myThread();
+        t1.start(); // start method internally call (run) methods declared in a thread
+        t2.start();
+
     }
 }
