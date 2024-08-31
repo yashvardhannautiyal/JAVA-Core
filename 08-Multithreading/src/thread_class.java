dc7e066839bf964f7bc45796c8f75055afbd58f3
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-31 22:37:09 +0530

    completed Thread(runnable r) method

--- 08-Multithreading/src/thread_class.java
+++ 08-Multithreading/src/thread_class.java
@@ -14,13 +14,31 @@ class myTh extends Thread{
     }
 }
 
+//2)Thread(Runnable r)
+class runnableTh implements Runnable{
+    public void run(){
+        int i =0;
+        while(i<9){
+            System.out.println("i am runnable interface ");
+            i++;
+        }
+    }
+}
+
+
 
 public class thread_class {
     public static void main(String[] args) {
         System.out.println("----------Thread class----------");
+//        Thread(String name)
         myTh thr = new myTh("holla");
         thr.start();
         System.out.println("id of thread : "+ thr.getId());
         System.out.println("name of thread : "+ thr.getName());
+
+//        Thread(Runnable r)
+        runnableTh bullet = new runnableTh();
+        Thread gun = new Thread(bullet);
+        gun.start();
     }
 }
