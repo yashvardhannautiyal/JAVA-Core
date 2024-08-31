eb37025e50017c79c77f4b6830d5c04c71fa3eda
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-31 22:31:51 +0530

    completed with Thread(String name)

--- 08-Multithreading/src/thread_class.java
+++ 08-Multithreading/src/thread_class.java
@@ -1,5 +1,26 @@
+//thread class constructos
+
+//1) Thread(String name)
+class myTh extends Thread{
+    public myTh(String name){
+        super(name);//this will call the method in the class which takes input of name
+    }
+    public void run(){
+        int i = 0;
+        while(i<=9){
+            System.out.println("i am a thread");
+            i++;
+        }
+    }
+}
+
+
 public class thread_class {
     public static void main(String[] args) {
         System.out.println("----------Thread class----------");
+        myTh thr = new myTh("holla");
+        thr.start();
+        System.out.println("id of thread : "+ thr.getId());
+        System.out.println("name of thread : "+ thr.getName());
     }
 }
