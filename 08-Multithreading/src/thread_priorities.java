963ba6251b989e8d13c4b10b0256a04ebe7ae1f7
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-08-31 23:16:40 +0530

    completed thread priorities

--- 08-Multithreading/src/thread_priorities.java
+++ 08-Multithreading/src/thread_priorities.java
@@ -5,8 +5,8 @@ class myTh extends Thread{
     }
     public void run(){
         int i = 0;
-        while(i<=9){
-            System.out.println("i am a thread");
+        while(i<=15){
+            System.out.println("i am a thread " + this.getName());
             i++;
         }
     }
@@ -21,7 +21,16 @@ public class thread_priorities {
         myTh t3 = new myTh("gutten tag");
         myTh t4 = new myTh("gutten morgan");
         myTh t5 = new myTh("gutten abend");
-        myTh t6 = new myTh("es geht");
+        myTh t6 = new myTh("es geht (most important)----------------");
+        t5.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.NORM_PRIORITY);
+        t1.start();
+        t6.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
 
     }
 }
