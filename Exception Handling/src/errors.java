b6bd69d4d3b43fa5a574a53194e2c84735c41230
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-05 17:45:03 +0530

    update errors.java

--- Exception Handling/src/errors.java
+++ Exception Handling/src/errors.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class errors {
     public static void main(String[] args) {
-        System.out.println("Errors in java");
+        System.out.println("-------Errors in java----");
+//        Syntax error
+//        int a = 9 //no semicolon
+//                a = a+3;
+//        d = 4; //no declaration defining data type of d
+
+
+//        Logical error : occurs due to some logical mistakes in program
+//        Ex
+//        int a= 9;
+//        int b = 8;
+//        int sum = a*b;//logical error
+
+//        Runtime error : exceptions occur due to bad input or resource constraints
+//        EX
+//        int a = 9;
+//        Scanner sc = new Scanner(System.in);
+//        int b = sc.nextInt();
+//        System.out.println("Sum = "+a+b); //user gives input 'h' error occurs
+
     }
+
 }
