0f048c10c77b76f6822bbbfb45d4eb54bad4de84
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-05 18:23:41 +0530

    completed try and catch

--- Exception Handling/src/tryCatchBlock.java
+++ Exception Handling/src/tryCatchBlock.java
@@ -1,5 +1,18 @@
 public class tryCatchBlock {
     public static void main(String[] args) {
         System.out.println("try catch block.....");
+        int a =6000;
+//        int b = 9;
+        int b = 0;
+        try {
+            int c=a/b;
+            System.out.println(c);
+        }
+        catch (Exception e){
+            System.out.println("we failed to devide");
+            System.out.println(e);
+        }
+        System.out.println("end of program"); //this line will execute even after the error occurs
+
     }
 }
