8fa6df1efb5859bd4116723306d992d7ff2ef463
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-07 20:23:01 +0530

    update finally_block.java

--- Exception Handling/src/finally_block.java
+++ Exception Handling/src/finally_block.java
@@ -1,5 +1,20 @@
 public class finally_block {
     public static void main(String[] args) {
-        System.out.println("finally block in java");
+        System.out.println("---------------finally block in java-----------");
+
+        try {
+           int a = 5;
+            int b =0;
+            int c = a/b;
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+        finally {//finally block will always run whether exception occurs or not
+            System.out.println("this is the end of program");
+            }
+
+
     }
 }
