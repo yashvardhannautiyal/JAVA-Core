4baed92cc8edda7d864e1a70cc764b04bb5ae66a
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-07 19:40:50 +0530

    update
    completed learning about throw and throws keyword in java

--- Exception Handling/src/throw_and_throws.java
+++ Exception Handling/src/throw_and_throws.java
@@ -1,5 +1,57 @@
+import java.util.SplittableRandom;
+
+class NegativeRadius extends Exception{
+    @Override
+    public String toString(){
+        return "radius cannot be negative";
+    }
+}
+
+
+
 public class throw_and_throws {
+    public static double area(int r) throws NegativeRadius{
+        if(r<0){
+            throw new NegativeRadius();
+        }
+        double result = Math.PI*r*r;
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+//    throws
+   public static int divide(int a,int b) throws ArithmeticException{
+        int result = a/b;
+        return result;
+    }
     public static void main(String[] args) {
         System.out.println("throw and throws keyword in java");
+
+        try {
+            int c = divide(6,0);
+            System.out.println(c);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    try {
+        double ar = area(7);
+        System.out.println(ar);
+
+    }
+    catch (Exception e){
+        System.out.println(e);
+    }
     }
 }
